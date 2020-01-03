f2b0af34def53427799795c5956574866ad3d304
subinan <060657@naver.com>
2020-01-03 13:40:39 +0900

    Initial Commit

---
+++ SeoulSportsCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeoulSportsCenter
+//
+//  Created by swuad_12 on 03/01/2020.
+//  Copyright © 2020 swuad_12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
